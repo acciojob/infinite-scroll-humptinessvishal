00662173600217529bf658b325bf1037efa1330a
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-09-21 14:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
+const ol = document.getElementById("infi-list");
+let i = 1
+for(i;i<=10;i++){
+	const li = document.createElement("li");
+	li.innerText = `Item ${i}`;
+	ol.append(li);
+}
 
+ol.addEventListener('scroll', function () {
+	if (ol.scrollTop + ol.clientHeight >= ol.scrollHeight) {
+		addListItem();
+	}
+});
+
+function addListItem() {
+	const newItem1 = document.createElement('li');
+	const newItem2 = document.createElement('li');
+    newItem1.textContent = `Item ${i}`
+	i++;
+	newItem2.textContent = `Item ${i}`
+	i++;
+    ol.appendChild(newItem1);
+	ol.appendChild(newItem2);
+}
